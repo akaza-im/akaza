528990fad090512b9518f4e3a4edd803be71ca02
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-09-03 23:42:47 +0900

    cleanup romaji table

--- combromkan.py
+++ combromkan.py
@@ -46,78 +46,6 @@ import re
 
 # This table is imported from KAKASI <http://kakasi.namazu.org/> and modified.
 
-KUNREITAB_H = """か      ka      が      ga      き      ki      きゃ      kya      きゅ      kyu 
-きょ      kyo      ぎ      gi      ぎゃ      gya      ぎゅ      gyu      ぎょ      gyo 
-く      ku      ぐ      gu      け      ke      げ      ge      こ      ko
-ご      go 
-
-さ      sa      ざ      za      し      si      しゃ      sya      しゅ      syu 
-しょ      syo      じ      zi      じゃ      zya      じゅ      zyu      じょ      zyo 
-す      su      ず      zu      せ      se      ぜ      ze      そ      so
-ぞ      zo 
-
-た      ta      だ      da      ち      ti      ちゃ      tya      ちゅ      tyu 
-ちょ      tyo      ぢ      di      ぢゃ      dya      ぢゅ      dyu      ぢょ      dyo 
-
-っ      xtu 
-っう゛      vvu      っう゛ぁ      vva      っう゛ぃ      vvi 
-っう゛ぇ      vve      っう゛ぉ      vvo 
-っか      kka      っが      gga      っき      kki      っきゃ      kkya 
-っきゅ      kkyu      っきょ      kkyo      っぎ      ggi      っぎゃ      ggya 
-っぎゅ      ggyu      っぎょ      ggyo      っく      kku      っぐ      ggu 
-っけ      kke      っげ      gge      っこ      kko      っご      ggo      っさ      ssa 
-っざ      zza      っし      ssi      っしゃ      ssya 
-っしゅ      ssyu      っしょ      ssyo 
-っじ      zzi      っじゃ      zzya      っじゅ      zzyu      っじょ      zzyo 
-っす      ssu      っず      zzu      っせ      sse      っぜ      zze      っそ      sso 
-っぞ      zzo      った      tta      っだ      dda      っち      tti 
-っちゃ      ttya      っちゅ      ttyu      っちょ      ttyo      っぢ      ddi 
-っぢゃ      ddya      っぢゅ      ddyu      っぢょ      ddyo      っつ      ttu 
-っづ      ddu      って      tte      っで      dde      っと      tto      っど      ddo 
-っは      hha      っば      bba      っぱ      ppa      っひ      hhi 
-っひゃ      hhya      っひゅ      hhyu      っひょ      hhyo      っび      bbi 
-っびゃ      bbya      っびゅ      bbyu      っびょ      bbyo      っぴ      ppi 
-っぴゃ      ppya      っぴゅ      ppyu      っぴょ      ppyo      っふ      hhu 
-っふぁ      ffa      っふぃ      ffi      っふぇ      ffe      っふぉ      ffo 
-っぶ      bbu      っぷ      ppu      っへ      hhe      っべ      bbe      っぺ    ppe
-っほ      hho      っぼ      bbo      っぽ      ppo      っや      yya      っゆ      yyu 
-っよ      yyo      っら      rra      っり      rri      っりゃ      rrya 
-っりゅ      rryu      っりょ      rryo      っる      rru      っれ      rre 
-っろ      rro 
-
-つ      tu      づ      du      て      te      で      de      と      to
-ど      do 
-
-な      na      に      ni      にゃ      nya      にゅ      nyu      にょ      nyo 
-ぬ      nu      ね      ne      の      no 
-
-は      ha      ば      ba      ぱ      pa      ひ      hi      ひゃ      hya 
-ひゅ      hyu      ひょ      hyo      び      bi      びゃ      bya      びゅ      byu 
-びょ      byo      ぴ      pi      ぴゃ      pya      ぴゅ      pyu      ぴょ      pyo 
-ふ      hu      ふぁ      fa      ふぃ      fi      ふぇ      fe      ふぉ      fo 
-ぶ      bu      ぷ      pu      へ      he      べ      be      ぺ      pe
-ほ      ho      ぼ      bo      ぽ      po 
-
-ま      ma      み      mi      みゃ      mya      みゅ      myu      みょ      myo 
-む      mu      め      me      も      mo 
-
-ゃ      xya      や      ya      ゅ      xyu      ゆ      yu      ょ      xyo
-よ      yo
-
-ら      ra      り      ri      りゃ      rya      りゅ      ryu      りょ      ryo 
-る      ru      れ      re      ろ      ro 
-
-ゎ      xwa      わ      wa      ゐ      wi      ゑ      we
-を      wo      ん      n 
-
-ん     n'
-でぃ   dyi
-ー     -
-ちぇ    tye
-っちぇ      ttye
-じぇ      zye
-"""
-
 HEPBURNTAB_H = """ぁ      xa      あ      a      ぃ      xi      い      i      ぅ      xu
 う      u      う゛      vu      う゛ぁ      va      う゛ぃ      vi      う゛ぇ      ve
 う゛ぉ      vo      ぇ      xe      え      e      ぉ      xo      お      o
@@ -129,14 +57,18 @@ HEPBURNTAB_H = """ぁ      xa      あ      a      ぃ      xi      い      i  
 ご      go      
 
 さ      sa      ざ      za      し      shi      しゃ      sha      しゅ      shu
+し      si      しゃ      sya      しゅ      syu 
 しょ      sho      じ      ji      じゃ      ja      じゅ      ju      じょ      jo
+しょ      syo      じ      zi      じゃ      zya      じゅ      zyu      じょ      zyo 
 す      su      ず      zu      せ      se      ぜ      ze      そ      so
 ぞ      zo
 
 た      ta      だ      da      ち      chi      ちゃ      cha      ちゅ      chu
+ち      ti      ちゃ      tya      ちゅ      tyu 
 ちょ      cho      ぢ      di      ぢゃ      dya      ぢゅ      dyu      ぢょ      dyo
+ちょ      tyo
 
-っ      xtsu      
+っ      xtsu      っ      xtu 
 っう゛      vvu      っう゛ぁ      vva      っう゛ぃ      vvi      
 っう゛ぇ      vve      っう゛ぉ      vvo      
 っか      kka      っが      gga      っき      kki      っきゃ      kkya      
@@ -144,17 +76,24 @@ HEPBURNTAB_H = """ぁ      xa      あ      a      ぃ      xi      い      i  
 っぎゅ      ggyu      っぎょ      ggyo      っく      kku      っぐ      ggu      
 っけ      kke      っげ      gge      っこ      kko      っご      ggo      っさ      ssa
 っざ      zza      っし      sshi      っしゃ      ssha      
+っし      ssi      っしゃ      ssya 
 っしゅ      sshu      っしょ      ssho      
+っしゅ      ssyu      っしょ      ssyo 
 っじ      jji      っじゃ      jja      っじゅ      jju      っじょ      jjo      
+っじ      zzi      っじゃ      zzya      っじゅ      zzyu      っじょ      zzyo 
 っす      ssu      っず      zzu      っせ      sse      っぜ      zze      っそ      sso
 っぞ      zzo      った      tta      っだ      dda      っち      cchi      
+っち      tti 
 っちゃ      ccha      っちゅ      cchu      っちょ      ccho      っぢ      ddi      
+っちゃ      ttya      っちゅ      ttyu      っちょ      ttyo
 っぢゃ      ddya      っぢゅ      ddyu      っぢょ      ddyo      っつ      ttsu      
+っつ      ttu 
 っづ      ddu      って      tte      っで      dde      っと      tto      っど      ddo
 っは      hha      っば      bba      っぱ      ppa      っひ      hhi      
 っひゃ      hhya      っひゅ      hhyu      っひょ      hhyo      っび      bbi      
 っびゃ      bbya      っびゅ      bbyu      っびょ      bbyo      っぴ      ppi      
 っぴゃ      ppya      っぴゅ      ppyu      っぴょ      ppyo      っふ      ffu      
+っふ      hhu 
 っふぁ      ffa      っふぃ      ffi      っふぇ      ffe      っふぉ      ffo      
 っぶ      bbu      っぷ      ppu      っへ      hhe      っべ      bbe      っぺ      ppe
 っほ      hho      っぼ      bbo      っぽ      ppo      っや      yya      っゆ      yyu
@@ -163,7 +102,8 @@ HEPBURNTAB_H = """ぁ      xa      あ      a      ぃ      xi      い      i  
 っろ      rro      
 
 つ      tsu      づ      du      て      te      で      de      と      to
-ど      do      
+つ      tu
+ど      do
 
 な      na      に      ni      にゃ      nya      にゅ      nyu      にょ      nyo
 ぬ      nu      ね      ne      の      no      
@@ -172,6 +112,7 @@ HEPBURNTAB_H = """ぁ      xa      あ      a      ぃ      xi      い      i  
 ひゅ      hyu      ひょ      hyo      び      bi      びゃ      bya      びゅ      byu
 びょ      byo      ぴ      pi      ぴゃ      pya      ぴゅ      pyu      ぴょ      pyo
 ふ      fu      ふぁ      fa      ふぃ      fi      ふぇ      fe      ふぉ      fo
+ふ      hu
 ぶ      bu      ぷ      pu      へ      he      べ      be      ぺ      pe
 ほ      ho      ぼ      bo      ぽ      po      
 
@@ -190,9 +131,9 @@ HEPBURNTAB_H = """ぁ      xa      あ      a      ぃ      xi      い      i  
 ん     n'
 でぃ   dyi
 ー     -
-ちぇ    che
-っちぇ      cche
-じぇ      je
+ちぇ    che ちぇ    tye
+っちぇ      cche っちぇ      ttye
+じぇ      je じぇ      zye
 
 てゃ	tha	てぃ	thi	てゅ	thu	てぇ	the	てょ	tho
 〜	z-	…	z.
@@ -212,7 +153,7 @@ def pairs(arr, size=2):
 
 ROMKAN_H = {}
 
-for kana, roma in pairs(re.split(r"\s+", KUNREITAB_H + HEPBURNTAB_H)):
+for kana, roma in pairs(re.split(r"\s+", HEPBURNTAB_H)):
     ROMKAN_H[roma] = kana
 
 # special modification
