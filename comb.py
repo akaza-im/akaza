893aa8e0b0c193445428caac14ebb1be13c90a38
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-09-05 00:41:19 +0900

    support 送りがな

--- comb.py
+++ comb.py
@@ -9,11 +9,17 @@ import marisa_trie
 import combromkan
 
 
-def parse_skkdict(path, encoding='euc-jp'):
-    result = {}
+def parse_skkdict(path: str, encoding: str = 'euc-jp'):
+    nasi = {}
+    ari = {}
+    target = ari
 
     with open(path, 'r', encoding=encoding) as fp:
         for line in fp:
+            if line == ";; okuri-ari entries.\n":
+                target = ari
+            if line == ";; okuri-nasi entries.\n":
+                target = nasi
             if line.startswith(';;'):
                 continue
 
@@ -23,16 +29,22 @@ def parse_skkdict(path, encoding='euc-jp'):
                 kanjis = kanjis.lstrip('/').rstrip('/').split('/')
                 kanjis = [re.sub(';.*', '', k) for k in kanjis]
 
-                result[yomi] = kanjis
+                target[yomi] = kanjis
 
-    return result
+    return ari, nasi
 
 
-def write_skkdict(outfname, dictionary, encoding='utf-8'):
+def write_skkdict(outfname, dict_ari, dict_nasi, encoding='utf-8'):
     with open(outfname, 'w', encoding=encoding) as ofh:
-        for yomi in sorted(dictionary.keys()):
+        ofh.write(";; okuri-ari entries.\n")
+        for yomi in sorted(dict_ari.keys()):
+            kanjis = dict_ari[yomi]
+            if len(kanjis) != 0:
+                ofh.write("%s /%s/\n" % (yomi, '/'.join(kanjis)))
 
-            kanjis = dictionary[yomi]
+        ofh.write(";; okuri-nasi entries.\n")
+        for yomi in sorted(dict_nasi.keys()):
+            kanjis = dict_nasi[yomi]
             if len(kanjis) != 0:
                 ofh.write("%s /%s/\n" % (yomi, '/'.join(kanjis)))
 
@@ -47,6 +59,9 @@ def merge_skkdict(dicts):
             result[k].extend(v)
 
     return result
+
+
+BOIN = set(['a', 'i', 'u', 'e', 'o'])
 
 
 class SystemDict:
@@ -66,8 +81,38 @@ class SystemDict:
             self.logger.info("loading dictionaries")
             t0 = time.time()
             t = []
-            dictionary = merge_skkdict([parse_skkdict(fname, encoding) for fname, encoding in dicts])
-            for k, v in dictionary.items():
+            # TODO cache trie.
+            dictionaries = [parse_skkdict(fname, encoding) for fname, encoding in dicts]
+
+            def expand_okuri(kana, kanjis):
+                if kana[-1].isalpha():
+                    if kana[-1] in BOIN:
+                        okuri = combromkan.to_hiragana(kana[-1])
+                        yield kana[:-1] + okuri, [kanji + okuri for kanji in kanjis]
+                    else:
+                        for b in BOIN:
+                            okuri = combromkan.to_hiragana(kana[-1] + b)
+                            yield kana[:-1] + okuri, [kanji + okuri for kanji in kanjis]
+                else:
+                    yield kana, kanjis
+
+
+            def ari2nasi(src):
+                retval = {}
+                for kana, kanjis in src.items():
+                    for kkk, vvv in expand_okuri(kana, kanjis):
+                        retval[kkk] = vvv
+                return retval
+
+            ari_dictionary = merge_skkdict([d[0] for d in dictionaries])
+            nasi_dictionary = merge_skkdict(
+                [d[1] for d in dictionaries] +
+                [ari2nasi(ari_dictionary)]
+            )
+
+            for k, v in nasi_dictionary.items():
+                if k.startswith('わたし'):
+                    print([k,v])
                 t.append((k, '/'.join(v).encode('utf-8')))
             self.trie = marisa_trie.BytesTrie(t)
             self.logger.info(f"LOADed JISYO: in {time.time() - t0:f} sec")
@@ -92,16 +137,16 @@ class UserDict:
         self.path = path
         self.logger = logger
         if os.path.isfile(path):
-            self.dict = parse_skkdict(path, encoding='utf-8')
+            self.dict_ari, self.dict_nasi = parse_skkdict(path, encoding='utf-8')
         else:
-            self.dict = {}
+            self.dict_ari, self.dict_nasi = {}, {}
 
     def get_candidates(self, src, hiragana):
         candidates = []
 
         for keyword in [src, hiragana]:
-            if keyword in self.dict:
-                got = self.dict[keyword]
+            if keyword in self.dict_nasi:
+                got = self.dict_nasi[keyword]
                 self.logger.debug("GOT: %s" % str(got))
                 for e in got:
                     candidates.append([e, e])
@@ -112,23 +157,23 @@ class UserDict:
         self.logger.info(f"add user_dict entry: roma='{roma}' kanji='{kanji}'")
         kana = combromkan.to_hiragana(roma)
 
-        if kana in self.dict:
-            e = self.dict[kana]
+        if kana in self.dict_nasi:
+            e = self.dict_nasi[kana]
             if kanji in e:
                 # イチバンマエにもっていく。
                 e.remove(kanji)
                 e.insert(0, kanji)
             else:
-                self.dict[kana].insert(0, kanji)
+                self.dict_nasi[kana].insert(0, kanji)
         else:
-            self.dict[kana] = [kanji]
+            self.dict_nasi[kana] = [kanji]
 
         # 非同期でかくようにしたほうが better.
         self.save()
-        self.logger.info("SAVED! %s" % str(self.dict))
+        self.logger.info("SAVED!")
 
     def save(self):
-        write_skkdict(self.path, self.dict)
+        write_skkdict(self.path, self.dict_ari, self.dict_nasi)
 
 
 class Comb:
@@ -177,8 +222,12 @@ if __name__ == '__main__':
     d = SystemDict()
     u = UserDict(os.path.join(configdir, 'user-dict.txt'))
     comb = Comb(logging.getLogger(__name__), u, d)
-    print(comb.convert('henkandekiru'))
+    # print(comb.convert('henkandekiru'))
+    print(comb.convert('watasi'))
     # print(comb.convert('hituyoudayo'))
-    print(list(d.get_candidates('henkandekiru', 'へんかんできる')))
+    # print(list(d.get_candidates('henkandekiru', 'へんかんできる')))
+    # print(list(d.get_candidates('warudakumi', 'わるだくみ')))
+    # print(list(d.get_candidates('subarasii', 'すばらしい')))
+    # print(list(d.get_candidates('watasi', 'わたし')))
     # print(list(d.get_candidates('hiragana', 'ひらがな')))
     # print(list(d.get_candidates('buffer', 'ぶっふぇr')))
