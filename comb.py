1c91101b07bcc2a9309feda7c5f5dcb5bdef4b7a
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-09-05 22:44:00 +0900

    split skkdict.py

--- comb.py
+++ comb.py
@@ -9,59 +9,7 @@ import pathlib
 import marisa_trie
 
 import combromkan
-
-
-def parse_skkdict(path: str, encoding: str = 'euc-jp'):
-    nasi = {}
-    ari = {}
-    target = ari
-
-    with open(path, 'r', encoding=encoding) as fp:
-        for line in fp:
-            if line == ";; okuri-ari entries.\n":
-                target = ari
-            if line == ";; okuri-nasi entries.\n":
-                target = nasi
-            if line.startswith(';;'):
-                continue
-
-            m = line.strip().split(' ', 1)
-            if len(m) == 2:
-                yomi, kanjis = m
-                kanjis = kanjis.lstrip('/').rstrip('/').split('/')
-                kanjis = [re.sub(';.*', '', k) for k in kanjis]
-
-                target[yomi] = kanjis
-
-    return ari, nasi
-
-
-def write_skkdict(outfname, dict_ari, dict_nasi, encoding='utf-8'):
-    with open(outfname, 'w', encoding=encoding) as ofh:
-        ofh.write(";; okuri-ari entries.\n")
-        for yomi in sorted(dict_ari.keys()):
-            kanjis = dict_ari[yomi]
-            if len(kanjis) != 0:
-                ofh.write("%s /%s/\n" % (yomi, '/'.join(kanjis)))
-
-        ofh.write(";; okuri-nasi entries.\n")
-        for yomi in sorted(dict_nasi.keys()):
-            kanjis = dict_nasi[yomi]
-            if len(kanjis) != 0:
-                ofh.write("%s /%s/\n" % (yomi, '/'.join(kanjis)))
-
-
-def merge_skkdict(dicts):
-    result = {}
-
-    for dic in dicts:
-        for k, v in dic.items():
-            if k not in result:
-                result[k] = []
-            result[k].extend(v)
-
-    return result
-
+from skkdict import parse_skkdict, write_skkdict, merge_skkdict
 
 BOIN = set(['a', 'i', 'u', 'e', 'o'])
 
---
+++ skkdict.py
@@ -0,0 +1,54 @@
+import re
+from typing import Dict, List
+
+
+def parse_skkdict(path: str, encoding: str = 'euc-jp'):
+    nasi: Dict[str, List[str]] = {}
+    ari: Dict[str, List[str]] = {}
+    target = ari
+
+    with open(path, 'r', encoding=encoding) as fp:
+        for line in fp:
+            if line == ";; okuri-ari entries.\n":
+                target = ari
+            if line == ";; okuri-nasi entries.\n":
+                target = nasi
+            if line.startswith(';;'):
+                continue
+
+            m = line.strip().split(' ', 1)
+            if len(m) == 2:
+                yomi: str = m[0]
+                kanjis: List[str] = m[1].lstrip('/').rstrip('/').split('/')
+                kanjis = [re.sub(';.*', '', k) for k in kanjis]
+
+                target[yomi] = kanjis
+
+    return ari, nasi
+
+
+def write_skkdict(outfname: str, dict_ari, dict_nasi, encoding: str = 'utf-8'):
+    with open(outfname, 'w', encoding=encoding) as ofh:
+        ofh.write(";; okuri-ari entries.\n")
+        for yomi in sorted(dict_ari.keys()):
+            kanjis = dict_ari[yomi]
+            if len(kanjis) != 0:
+                ofh.write("%s /%s/\n" % (yomi, '/'.join(kanjis)))
+
+        ofh.write(";; okuri-nasi entries.\n")
+        for yomi in sorted(dict_nasi.keys()):
+            kanjis = dict_nasi[yomi]
+            if len(kanjis) != 0:
+                ofh.write("%s /%s/\n" % (yomi, '/'.join(kanjis)))
+
+
+def merge_skkdict(dicts):
+    result = {}
+
+    for dic in dicts:
+        for k, v in dic.items():
+            if k not in result:
+                result[k] = []
+            result[k].extend(v)
+
+    return result
