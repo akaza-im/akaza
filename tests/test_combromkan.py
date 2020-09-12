baa17d9d0bcb2043ca50d6223c5cb0fe46c7d91c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-09-13 00:53:50 +0900

    support z,

--- tests/test_combromkan.py
+++ tests/test_combromkan.py
@@ -1,3 +1,5 @@
+import pytest
+
 from comb.combromkan import to_hiragana
 
 
@@ -10,3 +12,22 @@ def test_foo():
     assert to_hiragana('better') == 'べってr'
     assert to_hiragana('[') == '「'
     assert to_hiragana(']') == '」'
+
+
+@pytest.mark.parametrize('src, expected', [
+    ('a', 'あ'),
+    ('wo', 'を'),
+    ('du', 'づ'),
+    ('we', 'うぇ'),
+    ('di', 'ぢ'),
+    ('fu', 'ふ'),
+    ('ti', 'ち'),
+    ('wi', 'うぃ'),
+    ('we', 'うぇ'),
+    ('wo', 'を'),
+    ('z,', '‥'),
+    ('z.', '…'),
+])
+def test_bar(src, expected):
+    assert to_hiragana(src) == expected
+
