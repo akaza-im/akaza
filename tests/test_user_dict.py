1c80c509d99f45a9b9ea0622d84cd8d745eb1ff7
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-09-12 11:36:22 +0900

    use bigram based dict.

--- tests/test_user_dict.py
+++ tests/test_user_dict.py
@@ -19,3 +19,27 @@ def test_read():
     d.add_entry([Node(start_pos=0, word='熟語', yomi='じゅくご')])
     assert d.unigram == {'単語/たんご': 2, '熟語/じゅくご': 1}
     assert d.total == 3
+
+
+def test_read2():
+    tmpdir = TemporaryDirectory()
+    d = UserDict(tmpdir.name + "/foobar.dict")
+    d.add_entry([
+        Node(start_pos=0, word='私', yomi='わたし'),
+        Node(start_pos=1, word='だよ', yomi='だよ'),
+    ])
+    d.add_entry([
+        Node(start_pos=0, word='それは', yomi='それは'),
+        Node(start_pos=3, word='私', yomi='わたし'),
+        Node(start_pos=4, word='だよ', yomi='だよ'),
+    ])
+    d.add_entry([
+        Node(start_pos=0, word='私', yomi='わたし'),
+        Node(start_pos=1, word='です', yomi='です'),
+    ])
+
+    assert d.unigram == {'それは/それは': 1, 'だよ/だよ': 2, '私/わたし': 3, 'です/です': 1}
+    assert d.total == 7
+
+    assert d.bigram == {'それは/それは\t私/わたし': 1, '私/わたし\tだよ/だよ': 2, '私/わたし\tです/です': 1}
+    assert d.bigram_total == {'それは/それは': 1, '私/わたし': 3}
