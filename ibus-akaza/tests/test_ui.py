78740c3ab9144392d7f842ab1bbefa0e16412074
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-09-16 03:00:06 +0900

    分節の区切り方のバグを修正

--- ibus-akaza/tests/test_ui.py
+++ ibus-akaza/tests/test_ui.py
@@ -121,3 +121,29 @@ def test_extend_clause_left_most_left():
 
     # 2文節目が しい になっている
     assert 'たの/しい/じかん' == '/'.join([clause[0].yomi for clause in ui.clauses])
+
+
+def test_extend_clause_left_most_left_and_more():
+    ui = AkazaIBusEngine()
+    ui.preedit_string = "dondaketochikan"  # どん/だけ/とち/かん
+    ui.update_candidates()
+
+    # どん/だけ/とち/かん
+    print('/'.join([clause[0].yomi for clause in ui.clauses]))
+
+    # どん/だけ/とち/かん
+    # 0 1 2 3 4 5 6
+    # slice(0,2)
+    # slice(2,4)
+    # slice(4,7)
+    assert ui.current_clause == 0
+    ui.cursor_right()  # focus to だけ
+    assert ui.current_clause == 1
+    ui.cursor_right()  # focus to とち
+    assert ui.current_clause == 2
+    ui.extend_clause_right()  # とち→とちか
+    assert '/'.join([clause[0].yomi for clause in ui.clauses]) == 'どん/だけ/とちか/ん'
+    assert '/'.join(['どんだけとちかん'[s] for s in ui.force_selected_clause]) == 'どん/だけ/とちか/ん'
+    assert ui.current_clause == 2
+    ui.extend_clause_right()  # とちか→とちかん
+    assert '/'.join([clause[0].yomi for clause in ui.clauses]) == 'どん/だけ/とちかん'
