ed122f84429e7dbe57a3ff5f5fca5373e6c17d4a
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-09-17 07:36:12 +0900

    不要なエントリを wfreq ファイルから除外

--- akaza-data/bin/text2wfreq.py
+++ akaza-data/bin/text2wfreq.py
@@ -27,29 +27,40 @@ def split(a, n):
     return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
 
-numprocs = mp.cpu_count()
+def main():
+    numprocs = mp.cpu_count()
 
-files = glob.glob('dat/*/wiki_*')
-chunks = split(files, numprocs)
+    files = glob.glob('dat/*/wiki_*')
+    chunks = split(files, numprocs)
 
-result_pool = []
-pool = mp.Pool(numprocs)
+    result_pool = []
+    pool = mp.Pool(numprocs)
 
-for chunk in chunks:
-    result_pool.append(pool.apply_async(worker, args=(chunk,)))
+    for chunk in chunks:
+        result_pool.append(pool.apply_async(worker, args=(chunk,)))
 
-merged_wfreq = {}
-while len(result_pool) > 0:
-    print(f"Remains: {len(result_pool)}")
-    for r in result_pool:
-        if r.ready():
-            print("LOOP!")
-            wfreq_part = r.get()
-            for k, v in wfreq_part.items():
-                merged_wfreq[k] = merged_wfreq.get(k, 0) + v
-            result_pool.remove(r)
-    time.sleep(0.1)
+    merged_wfreq = {}
+    while len(result_pool) > 0:
+        print(f"Remains: {len(result_pool)}")
+        for r in result_pool:
+            if r.ready():
+                wfreq_part = r.get()
+                for k, v in wfreq_part.items():
+                    merged_wfreq[k] = merged_wfreq.get(k, 0) + v
+                result_pool.remove(r)
+        time.sleep(0.1)
 
-with open('jawiki.wfreq', 'w') as wfp:
-    for k, v in merged_wfreq.items():
-        wfp.write(f"{k} {v}\n")
+    with open('jawiki.wfreq', 'w') as wfp:
+        for key in sorted(merged_wfreq.keys()):
+            count = merged_wfreq[key]
+            if key == '//':
+                continue
+            if '/' not in key:
+                continue
+            if key.endswith('/UNK'):
+                continue
+            wfp.write(f"{key} {count}\n")
+
+
+if __name__ == '__main__':
+    main()
