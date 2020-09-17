20bb7270ed12d6ca6c4d41da4c307276d37397f7
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-09-18 05:36:37 +0900

    cleanup

--- ibus-akaza/ibus_akaza/input_mode.py
+++ ibus-akaza/ibus_akaza/input_mode.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class InputMode:
     #    input_modes = {
     #        u'InputMode.Hiragana': (INPUT_MODE_HIRAGANA, 'あ'),
@@ -7,19 +10,26 @@ class InputMode:
     # u'InputMode.WideLatin' : (INPUT_MODE_WIDE_LATIN, 'Ａ'),
     #    }
 
-    def __init__(self, prop_name: str, mode_code: int, symbol: str):
+    def __init__(self, prop_name: str, mode_code: int, symbol: str, label: str):
         self.prop_name = prop_name
         self.mode_code = mode_code
         self.symbol = symbol
+        self.label = label
 
     def __eq__(self, other):
         return self.mode_code == other.mode_code
 
 
-INPUT_MODE_HIRAGANA = InputMode('InputMode.Hiragana', 0, 'あ')
-INPUT_MODE_LATIN = InputMode('InputMode.Latin', 1, '_A')
+INPUT_MODE_HIRAGANA = InputMode('InputMode.Hiragana', 0, 'あ', 'ひらがな')
+INPUT_MODE_LATIN = InputMode('InputMode.Latin', 1, '_A', 'Latin')
 
-_INPUT_MODE_PROP_NAME2MODE = dict([(mode.prop_name, mode) for mode in [INPUT_MODE_HIRAGANA, INPUT_MODE_LATIN]])
+_ALL_INPUT_MODE = [INPUT_MODE_HIRAGANA, INPUT_MODE_LATIN]
+
+_INPUT_MODE_PROP_NAME2MODE = dict([(mode.prop_name, mode) for mode in _ALL_INPUT_MODE])
+
+
+def get_all_input_modes() -> List[InputMode]:
+    return _ALL_INPUT_MODE
 
 
 def get_input_mode_from_prop_name(prop_code: str):
