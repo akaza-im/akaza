6628c8d7e0908c030dd75beec7a40f87bac82728
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-09-18 17:41:00 +0900

    path fix

--- ibus-akaza/tests/test_input_mode.py
+++ ibus-akaza/tests/test_input_mode.py
@@ -1,8 +1,10 @@
 from ibus_akaza.input_mode import *
 
+
 def test_input_mode():
     assert INPUT_MODE_HIRAGANA == INPUT_MODE_HIRAGANA
     assert INPUT_MODE_HIRAGANA != INPUT_MODE_ALNUM
+
 
 def test_input_mode_by_prop_name():
     assert get_input_mode_from_prop_name('InputMode.Hiragana') == INPUT_MODE_HIRAGANA
