f4b50267e16d39de36016243dffc60122941731d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-09-27 12:27:27 +0900

    add signature to dump files

--- akaza-data/utils/dump-system-dict.py
+++ akaza-data/utils/dump-system-dict.py
@@ -1,12 +1,17 @@
+import datetime
 import pathlib
+import subprocess
 
 import marisa_trie
+
+hash = subprocess.run(["git", "rev-parse", "--short", 'HEAD'], capture_output=True).stdout.decode('utf-8')
+sig = datetime.datetime.now().strftime('%Y%m%d-%H%M') + "-" + hash
 
 trie = marisa_trie.BytesTrie()
 trie.load('akaza_data/data/system_dict.trie')
 
 pathlib.Path('work/dump').mkdir(exist_ok=True, parents=True)
-with open(f"work/dump/dict.txt", 'w') as wfp:
+with open(f"work/dump/{sig}-dict.txt", 'w') as wfp:
     for yomi, kanji_bytes in trie.items():
         kanjis = kanji_bytes.decode('utf-8')
-        wfp.write(f"{yomi} {kanjis}\n")
+        wfp.write(f"{yomi} /{kanjis}/\n")
