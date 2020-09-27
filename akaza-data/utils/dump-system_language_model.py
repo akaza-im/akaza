7a558e6f6dafac98423198ce9f9a8773281a53ca
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-09-27 12:27:27 +0900

    add signature to dump files

--- akaza-data/utils/dump-system_language_model.py
+++ akaza-data/utils/dump-system_language_model.py
@@ -1,9 +1,11 @@
 import pathlib
+import subprocess
+import datetime
 
 import marisa_trie
 
 
-def process(type_name: str):
+def process(type_name: str, sig):
     print(f"Type: {type_name}")
 
     trie = marisa_trie.RecordTrie('<f')
@@ -11,15 +13,18 @@ def process(type_name: str):
     print(f"Len: {type_name} {len(trie)}")
 
     pathlib.Path('work/dump').mkdir(exist_ok=True, parents=True)
-    with open(f"work/dump/{type_name}.txt", 'w') as wfp:
+    with open(f"work/dump/{sig}-{type_name}.txt", 'w') as wfp:
         for key, score in trie.items():
             wfp.write(f"{key} {score}\n")
 
 
 def main():
-    process('1gram')
-    process('2gram')
-    process('3gram')
+    hash = subprocess.run(["git", "rev-parse", "--short", 'HEAD'], capture_output=True).stdout.decode('utf-8')
+    sig = datetime.datetime.now().strftime('%Y%m%d-%H%M') + "-" + hash
+
+    process('1gram', sig)
+    process('2gram', sig)
+    process('3gram', sig)
 
 
 if __name__ == '__main__':
