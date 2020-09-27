8a69a8693fab5e17a1c4a0d166f5256f9c6596e4
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-09-27 12:02:26 +0900

    Cleanup utils

--- akaza-data/utils/dump-system-dict.py
+++ akaza-data/utils/dump-system-dict.py
@@ -1,8 +1,12 @@
+import pathlib
+
 import marisa_trie
 
 trie = marisa_trie.BytesTrie()
 trie.load('akaza_data/data/system_dict.trie')
 
-for yomi, kanji_bytes in trie.items():
-    kanjis = kanji_bytes.decode('utf-8')
-    print(f"{yomi} {kanjis}")
+pathlib.Path('work/dump').mkdir(exist_ok=True, parents=True)
+with open(f"work/dump/dict.txt", 'w') as wfp:
+    for yomi, kanji_bytes in trie.items():
+        kanjis = kanji_bytes.decode('utf-8')
+        wfp.write(f"{yomi} {kanjis}\n")
--- akaza-data/utils/dump-system_language_model.py
+++ akaza-data/utils/dump-system_language_model.py
@@ -1,7 +1,26 @@
+import pathlib
+
 import marisa_trie
 
-trie = marisa_trie.RecordTrie('<f')
-trie.mmap('akaza_data/data/system_language_model.trie')
 
-for key, score in trie.items():
-    print(f"{key} {score}")
+def process(type_name: str):
+    print(f"Type: {type_name}")
+
+    trie = marisa_trie.RecordTrie('<f')
+    trie.mmap(f'akaza_data/data/system_language_model.{type_name}.trie')
+    print(f"Len: {type_name} {len(trie)}")
+
+    pathlib.Path('work/dump').mkdir(exist_ok=True, parents=True)
+    with open(f"work/dump/{type_name}.txt", 'w') as wfp:
+        for key, score in trie.items():
+            wfp.write(f"{key} {score}\n")
+
+
+def main():
+    process('1gram')
+    process('2gram')
+    process('3gram')
+
+
+if __name__ == '__main__':
+    main()
