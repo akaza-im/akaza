f38d3603a205ebbb9bcec72e5ed44b8cd60dc18d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-10-03 22:01:27 +0900

    fix ci case

--- akaza-data/bin/080_system_dump.py
+++ akaza-data/bin/080_system_dump.py
@@ -14,27 +14,10 @@ def dump_system_dict(sig):
             wfp.write(f"{yomi} /{kanjis}/\n")
 
 
-def dump_system_lm(type_name: str, sig):
-    print(f"Type: {type_name}")
-
-    trie = marisa_trie.RecordTrie('<f')
-    trie.mmap(f'akaza_data/data/system_language_model.{type_name}.trie')
-    print(f"Len: {type_name} {len(trie)}")
-
-    mkdir_p('work/dump')
-    with open(f"work/dump/{sig}-{type_name}.txt", 'w') as wfp:
-        for key, score in trie.items():
-            wfp.write(f"{key} {score}\n")
-
-
 def main():
     sig = get_sig()
 
     dump_system_dict(sig)
-
-    dump_system_lm('1gram', sig)
-    dump_system_lm('2gram', sig)
-    dump_system_lm('3gram', sig)
 
 
 if __name__ == '__main__':
