10c238a2fe346120dc8690cb6f12665676d9eebc
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-10-05 12:48:58 +0900

    fix test

--- ibus-akaza/tests/test_skk_file_dict.py
+++ ibus-akaza/tests/test_skk_file_dict.py
@@ -1,12 +1,9 @@
 import pathlib
-from tempfile import TemporaryDirectory
 
-from akaza.skk_file_dict import load_skk_file_dict
+from ibus_akaza.skk_file_dict import load_skk_file_dict
 
 
 def test_read2():
-    tmpdir = TemporaryDirectory()
-
     path = str(pathlib.Path(__file__).parent.joinpath('data', 'SKK-JISYO.test'))
     d = load_skk_file_dict(path)
     assert d.find_kanjis('たばた') == ['田端']
