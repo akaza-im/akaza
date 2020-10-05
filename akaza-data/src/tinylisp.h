c14509f3f6f227d17922f9dcc373e7b31138bda6
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-10-06 00:47:10 +0900

    tinylisp

---
+++ akaza-data/src/tinylisp.h
@@ -0,0 +1,126 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace akaza {
+namespace tinylisp {
+
+enum NodeType { NODE_LIST, NODE_SYMBOL, NODE_STRING };
+
+class Node {
+private:
+  NodeType _type;
+
+protected:
+  Node(NodeType type) { this->_type = type; }
+
+public:
+  NodeType type() { return this->_type; }
+};
+
+class ListNode : public Node {
+private:
+  std::vector<std::shared_ptr<Node>> _children;
+
+public:
+  ListNode(std::vector<std::shared_ptr<Node>> children) : Node(NODE_LIST) {
+    this->_children = children;
+  }
+
+  std::vector<std::shared_ptr<Node>> *children() { return &_children; }
+};
+
+class StringNode : public Node {
+private:
+  std::string _str;
+
+public:
+  StringNode(std::string str) : Node(NODE_STRING) { this->_str = str; }
+  std::string str() { return _str; }
+};
+
+class SymbolNode : public Node {
+private:
+  std::string _symbol;
+
+public:
+  SymbolNode(std::string symbol) : Node(NODE_SYMBOL) { this->_symbol = symbol; }
+  std::string symbol() { return _symbol; }
+};
+
+class TinyLisp {
+private:
+  std::vector<std::string> split(const std::string &s) {
+    std::vector<std::string> elems;
+    std::stringstream ss(s);
+    std::string item;
+    while (getline(ss, item, ' ')) {
+      if (!item.empty()) {
+        elems.push_back(item);
+      }
+    }
+    return elems;
+  }
+
+  std::string replace_string(std::string target, std::string pattern,
+                             std::string format) {
+    std::string::size_type pos(target.find(pattern));
+
+    while (pos != std::string::npos) {
+      target.replace(pos, pattern.length(), format);
+      pos = target.find(pattern, pos + format.length());
+    }
+
+    return target;
+  }
+
+  std::vector<std::string> tokenize(std::string src) {
+    src = replace_string(src, "(", " ( ");
+    src = replace_string(src, ")", " ) ");
+    return split(src);
+  }
+
+  std::shared_ptr<Node> _read_from(std::vector<std::string> &tokens,
+                                   int depth) {
+    if (tokens.size() == 0) {
+      throw std::runtime_error("Unexpected EOF while reading(LISP)");
+    }
+    std::string token = tokens[0];
+    tokens.erase(tokens.begin());
+    if (token == "(") {
+      std::vector<std::shared_ptr<Node>> values;
+      while (tokens[0] != ")") {
+        values.push_back(_read_from(tokens, depth + 1));
+      }
+      tokens.erase(tokens.begin()); // pop off ")"
+      return std::shared_ptr<Node>(new ListNode(values));
+    } else if (token == ")") {
+      throw std::runtime_error("Unexpected ')'");
+    } else {
+      return _atom(token);
+    }
+  }
+
+  std::shared_ptr<Node> _atom(std::string token) {
+    if (token.size() > 0 && token[0] == '"') {
+      return std::shared_ptr<Node>(
+          new StringNode(token.substr(1, token.size() - 2)));
+    } else {
+      return std::shared_ptr<Node>(new SymbolNode(token));
+    }
+  }
+
+public:
+  std::shared_ptr<Node> parse(std::string src) {
+    auto tokens = tokenize(src);
+    return _read_from(tokens, 0);
+  }
+};
+
+} // namespace tinylisp
+} // namespace akaza
