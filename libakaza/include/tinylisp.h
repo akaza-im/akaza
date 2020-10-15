99181ac0735836b086d6754885d11027a79576fc
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-10-15 11:03:26 +0900

    more wstring

--- libakaza/include/tinylisp.h
+++ libakaza/include/tinylisp.h
@@ -49,22 +49,22 @@ namespace akaza {
 
         class StringNode : public Node {
         private:
-            std::string _str;
+            std::wstring str_;
 
         public:
-            StringNode(std::string str) : Node(NODE_STRING) { this->_str = str; }
+            StringNode(const std::wstring &str) : Node(NODE_STRING) { this->str_ = str; }
 
-            std::string str() { return _str; }
+            std::wstring str() { return str_; }
         };
 
         class SymbolNode : public Node {
         private:
-            std::string _symbol;
+            std::wstring symbol_;
 
         public:
-            SymbolNode(std::string symbol) : Node(NODE_SYMBOL) { this->_symbol = symbol; }
+            SymbolNode(const std::wstring &symbol) : Node(NODE_SYMBOL) { this->symbol_ = symbol; }
 
-            std::string symbol() { return _symbol; }
+            std::wstring symbol() { return symbol_; }
         };
 
         using function_node_func =
@@ -95,11 +95,11 @@ namespace akaza {
 
         class TinyLisp {
         private:
-            static std::vector<std::string> split(const std::string &s) {
-                std::vector<std::string> elems;
-                std::stringstream ss(s);
-                std::string item;
-                while (getline(ss, item, ' ')) {
+            static std::vector<std::wstring> split(const std::wstring &s) {
+                std::vector<std::wstring> elems;
+                std::wstringstream ss(s);
+                std::wstring item;
+                while (getline(ss, item, L' ')) {
                     if (!item.empty()) {
                         elems.push_back(item);
                     }
@@ -107,8 +107,8 @@ namespace akaza {
                 return elems;
             }
 
-            std::string replace_string(std::string target, std::string pattern,
-                                       std::string format) const {
+            std::wstring replace_string(std::wstring target, std::wstring pattern,
+                                        std::wstring format) const {
                 std::string::size_type pos(target.find(pattern));
 
                 while (pos != std::string::npos) {
@@ -119,56 +119,32 @@ namespace akaza {
                 return target;
             }
 
-            std::vector<std::string> tokenize(std::string src) const {
-                src = replace_string(src, "(", " ( ");
-                src = replace_string(src, ")", " ) ");
-                return split(src);
+            std::vector<std::wstring> tokenize(const std::wstring &src) const {
+                std::wstring buf = src;
+                buf = replace_string(buf, L"(", L" ( ");
+                buf = replace_string(buf, L")", L" ) ");
+                return split(buf);
             }
 
-            std::shared_ptr<Node> _read_from(std::vector<std::string> &tokens,
-                                             int depth) const {
-                if (tokens.size() == 0) {
-                    throw std::runtime_error("Unexpected EOF while reading(LISP)");
-                }
-                std::string token = tokens[0];
-                tokens.erase(tokens.begin());
-                if (token == "(") {
-                    std::vector<std::shared_ptr<Node>> values;
-                    while (tokens[0] != ")") {
-                        values.push_back(_read_from(tokens, depth + 1));
-                    }
-                    tokens.erase(tokens.begin()); // pop off ")"
-                    return std::shared_ptr<Node>(new ListNode(values));
-                } else if (token == ")") {
-                    throw std::runtime_error("Unexpected ')'");
-                } else {
-                    return _atom(token);
-                }
-            }
+            std::shared_ptr<Node> _read_from(std::vector<std::wstring> &tokens,
+                                             int depth) const;
 
-            std::shared_ptr<Node> _atom(std::string token) const {
-                if (token.size() > 0 && token[0] == '"') {
-                    return std::shared_ptr<Node>(
-                            new StringNode(token.substr(1, token.size() - 2)));
-                } else {
-                    return std::shared_ptr<Node>(new SymbolNode(token));
-                }
-            }
+            std::shared_ptr<Node> _atom(const std::wstring &token) const;
 
         public:
-            std::shared_ptr<Node> parse(std::string src) const {
+            std::shared_ptr<Node> parse(const std::wstring &src) const {
                 auto tokens = tokenize(src);
                 return _read_from(tokens, 0);
             }
 
             std::shared_ptr<Node> eval(std::shared_ptr<Node> x) const;
 
-            std::string run(std::string sexp) const {
+            std::wstring run(const std::wstring &sexp) const {
                 std::shared_ptr<Node> node = this->run_node(sexp);
                 return static_cast<StringNode *>(&*node)->str();
             }
 
-            std::shared_ptr<Node> run_node(std::string sexp) const {
+            std::shared_ptr<Node> run_node(const std::wstring &sexp) const {
                 return this->eval(parse(sexp));
             }
         };
--- libakaza/t/03_tinylisp.cc
+++ libakaza/t/03_tinylisp.cc
@@ -10,32 +10,32 @@ int main() {
     akaza::tinylisp::TinyLisp tinylisp;
 
     {
-        std::shared_ptr<Node> got = tinylisp.parse("(a \"abc\")");
+        std::shared_ptr<Node> got = tinylisp.parse(L"(a \"abc\")");
 
         ok(got->type() == NODE_LIST);
         ok(dynamic_cast<ListNode*>(&*got)->children()->size() == 2);
 
         auto a = dynamic_cast<ListNode*>(&*got)->children()->at(0);
         ok(a->type() == NODE_SYMBOL);
-        ok(dynamic_cast<SymbolNode*>(&*a)->symbol() == "a");
+        ok(dynamic_cast<SymbolNode*>(&*a)->symbol() == L"a");
 
         auto abc = dynamic_cast<ListNode*>(&*got)->children()->at(1);
         ok(abc->type() == NODE_STRING);
-        ok(dynamic_cast<StringNode*>(&*abc)->str() == "abc");
+        ok(dynamic_cast<StringNode*>(&*abc)->str() == L"abc");
     }
 
     {
-        std::shared_ptr<Node> got = tinylisp.run_node("(strftime (current-datetime) \"%Y-%m-%d\")");
+        std::shared_ptr<Node> got = tinylisp.run_node(L"(strftime (current-datetime) \"%Y-%m-%d\")");
         ok(got->type() == NODE_STRING);
-        std::string got_str = dynamic_cast<StringNode*>(&*got)->str();
-        note("%s", got_str.c_str());
+        std::wstring got_str = dynamic_cast<StringNode*>(&*got)->str();
+        // note("%s", got_str.c_str());
     }
 
     {
-        std::shared_ptr<Node> got = tinylisp.run_node(R"((. "a" "b"))");
+        std::shared_ptr<Node> got = tinylisp.run_node(LR"((. "a" "b"))");
         ok(got->type() == NODE_STRING);
-        std::string got_str = dynamic_cast<StringNode*>(&*got)->str();
-        ok(got_str == "ab");
+        std::wstring got_str = dynamic_cast<StringNode*>(&*got)->str();
+        ok(got_str == L"ab");
     }
 
     done_testing();
