616ddc3d6cbdf7103b0cd50da9b8e24e1022afd9
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-10-19 15:58:25 +0900

    refactor

--- libakaza/src/system_lm.cc
+++ libakaza/src/system_lm.cc
@@ -2,6 +2,8 @@
 #include "../include/system_lm.h"
 #include <locale>
 #include <codecvt>
+#include <cstring>
+#include <iostream>
 
 void akaza::SystemUnigramLM::load(const char *path) {
     trie_.load(path);
@@ -48,4 +50,52 @@ void akaza::SystemBigramLM::load(const char *path) {
     trie_.load(path);
     D(std::cout << "Loading SystemBigramLM " << path << " size: " << trie_.size()
                 << " " << __FILE__ << ":" << __LINE__ << std::endl);
+}
+
+float akaza::SystemBigramLM::find_bigram(int32_t word_id1, int32_t word_id2) const {
+    uint32_t uword_id1 = word_id1;
+    uint32_t uword_id2 = word_id2;
+    uint8_t idbuf[4];
+    std::string query;
+    std::memcpy(idbuf, &uword_id1, sizeof(word_id1));
+    query += std::string(idbuf, idbuf + 3);
+    std::memcpy(idbuf, &uword_id2, sizeof(word_id2));
+    query += std::string(idbuf, idbuf + 3);
+
+    marisa::Agent agent;
+    agent.set_query(query.c_str(), query.size());
+
+    while (trie_.predictive_search(agent)) {
+        const char *p = agent.key().ptr() + query.size();
+        float score;
+        std::memcpy(&score, p, sizeof(float));
+        return score;
+    }
+    return 0;
+}
+
+void akaza::SystemUnigramLMBuilder::add(const std::string &word, float score) {
+    char buf[sizeof(float)];
+    memcpy(buf, &score, sizeof(float));
+    std::string key(word + "\xff" + std::string(buf, sizeof(float)));
+    keyset_.push_back(key.c_str(), key.size());
+}
+
+void akaza::SystemBigramLMBuilder::add(int32_t word_id1, int32_t word_id2, float score) {
+    // ここで処理する。
+    std::string keybuf;
+    uint8_t idbuf[sizeof(int32_t)];
+    char scorebuf[sizeof(float)];
+
+    // packed ID     # 3 bytes(24bit). 最大語彙: 8,388,608
+    std::memcpy(idbuf, &word_id1, sizeof(word_id1));
+    keybuf += std::string(idbuf, idbuf + 3);
+    std::memcpy(idbuf, &word_id2, sizeof(word_id2));
+    keybuf += std::string(idbuf, idbuf + 3);
+
+    // packed float  # score: 4 bytes
+    std::memcpy(scorebuf, &score, sizeof(score));
+    keybuf += std::string(scorebuf, scorebuf + 4);
+
+    keyset_.push_back(keybuf.c_str(), keybuf.size());
 }
