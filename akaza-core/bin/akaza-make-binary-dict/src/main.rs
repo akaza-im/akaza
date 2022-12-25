07725c7b48c3fad8aef612463693d6cb1d3e5350
Tokuhiro Matsuno <tokuhirom@gmail.com>
2022-12-26 00:40:18 +0900

    refactoring

--- akaza-core/bin/akaza-make-binary-dict/src/main.rs
+++ akaza-core/bin/akaza-make-binary-dict/src/main.rs
@@ -1,30 +1,27 @@
 use std::env;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
-use rx_sys::RXBuilder;
+use libakaza::trie::TrieBuilder;
 
 unsafe fn make_binary_dict(txtfile: &String, triefile: &String) {
     println!("Generating {} from {}", triefile, txtfile);
 
-    let rx_builder = RXBuilder::new();
+    let trie_builder = TrieBuilder::new();
 
     let file = File::open(txtfile).expect("Open {txtfile} correctly.");
     for line in BufReader::new(file).lines() {
         let line = line.unwrap();
         let v: Vec<&str> = line.trim().split(" ").collect();
+        if v.len() != 2 {
+            break;
+        }
         let word = v[0];
         let kanjis = v[1];
         println!("word={} kanjis={}", word, kanjis);
         let key = [word.as_bytes(), b"\xff", kanjis.as_bytes(), b"\x00"].concat();
-        rx_builder.add(key);
+        trie_builder.add(key);
     }
-    rx_builder.build();
-    let image = rx_builder.get_image();
-    let size = rx_builder.get_size();
-    let image = std::slice::from_raw_parts(image, size as usize);
-
-    let mut ofile = File::create(triefile).unwrap();
-    ofile.write_all(image).unwrap();
+    trie_builder.save(triefile);
 }
 
 fn main() {
--- akaza-core/libakaza/src/lib.rs
+++ akaza-core/libakaza/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod trie;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
