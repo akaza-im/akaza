cc6719c9bb6f705050dbf5fcde9444096c64ab80
Tokuhiro Matsuno <tokuhirom@gmail.com>
2022-12-27 23:45:18 +0900

    remove rx-sys. I use marisa!

--- akaza-core/rx-sys/README.md
+++
@@ -1,3 +0,0 @@
-rx の rust bindings です。
-
-lib/bindings.rs は bindgen から生成したものをベースにしています。
--- akaza-core/rx-sys/build.rs
+++
@@ -1,8 +0,0 @@
-extern crate cc;
-
-fn main() {
-    cc::Build::new()
-        .file("rx/rx.c")
-        .include("rx")
-        .compile("rx");
-}
