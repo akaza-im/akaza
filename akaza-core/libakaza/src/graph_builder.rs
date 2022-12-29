8a28c018cbfb9b029ed085fb928570f64c86a088
Tokuhiro Matsuno <tokuhirom@gmail.com>
2022-12-30 01:10:44 +0900

    builder

--- akaza-core/libakaza/src/graph_builder.rs
+++ akaza-core/libakaza/src/graph_builder.rs
@@ -1,7 +1,6 @@
-use std::borrow::{Borrow, BorrowMut};
-use std::cell::RefCell;
+use log::trace;
 use std::collections::{HashMap, HashSet};
-use std::rc::Rc;
+use std::hash::{Hash, Hasher};
 
 use marisa_sys::{Keyset, Marisa};
 
@@ -10,11 +9,26 @@ use marisa_sys::{Keyset, Marisa};
 
 #[derive(PartialEq)]
 struct WordNode {
-    start_pos: usize,
+    start_pos: i32,
     kanji: String,
     cost: f32,
-    prev: Option<Rc<RefCell<WordNode>>>,
 }
+/*
+impl PartialEq<Self> for WordNode {
+    fn eq(&self, other: &Self) -> bool {
+        self.start_pos == other.start_pos && self.kanji == other.kanji && self.cost == other.cost
+    }
+}
+*/
+impl Hash for WordNode {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.start_pos.hash(state);
+        self.kanji.hash(state);
+        u32::from_le_bytes(self.cost.to_le_bytes()).hash(state);
+    }
+}
+
+impl Eq for WordNode {}
 
 impl WordNode {
     fn create_bos() -> WordNode {
@@ -22,30 +36,20 @@ impl WordNode {
             start_pos: 0,
             kanji: "__BOS__".to_string(),
             cost: 0_f32,
-            prev: None,
         }
     }
-    fn create_eos() -> WordNode {
+    fn create_eos(yomi: &String) -> WordNode {
         WordNode {
-            start_pos: 0,
+            start_pos: yomi.len() as i32,
             kanji: "__EOS__".to_string(),
             cost: 0_f32,
-            prev: None,
         }
     }
-    fn new(start_pos: usize, kanji: &String) -> WordNode {
+    fn new(start_pos: i32, kanji: &String) -> WordNode {
         WordNode {
             start_pos,
             kanji: kanji.clone(),
             cost: 0_f32,
-            prev: None,
-        }
-    }
-
-    pub fn set_prev(&mut self, prev: Option<Rc<RefCell<WordNode>>>) {
-        match prev {
-            Some(thing) => self.prev = Some(Rc::clone(&thing)),
-            None => self.prev = None,
         }
     }
 }
@@ -163,7 +167,7 @@ impl Segmenter {
 
 // 次に必要なのは、分割された文字列から、グラフを構築する仕組みである。
 struct GraphResolver {
-    graph: HashMap<usize, Vec<Rc<RefCell<WordNode>>>>,
+    graph: HashMap<i32, Vec<WordNode>>,
 }
 
 impl GraphResolver {
@@ -175,83 +179,101 @@ impl GraphResolver {
     fn construct_graph(
         yomi: &String,
         words_ends_at: HashMap<usize, Vec<String>>,
-    ) -> HashMap<usize, Vec<Rc<RefCell<WordNode>>>> {
-        let mut graph: HashMap<usize, Vec<Rc<RefCell<WordNode>>>> = HashMap::new();
-        graph.insert(0, vec![Rc::new(RefCell::new(WordNode::create_bos()))]);
-        graph.insert(
-            yomi.len() + 1,
-            vec![Rc::new(RefCell::new(WordNode::create_eos()))],
-        );
+    ) -> HashMap<i32, Vec<WordNode>> {
+        // このグラフのインデクスは単語の終了位置。
+        let mut graph: HashMap<i32, Vec<WordNode>> = HashMap::new();
+        graph.insert(0, vec![WordNode::create_bos()]);
+        graph.insert((yomi.len() + 1) as i32, vec![WordNode::create_eos(yomi)]);
 
         for (end_pos, yomis) in words_ends_at {
             for yomi in yomis {
                 // ↓ここで start_pos 渡す意味ある?
-                let node = WordNode::new(end_pos - yomi.len(), &yomi);
+                let node = WordNode::new((end_pos - yomi.len()) as i32, &yomi);
                 // ほんとうは、そもそもここで、漢字に変換した結果を詰め込まないといけない。
 
-                let vec = graph.entry(end_pos).or_insert(Vec::new());
-                vec.push(Rc::new(RefCell::new(node)));
+                let vec = graph.entry(end_pos as i32).or_insert(Vec::new());
+                vec.push(node);
             }
         }
         return graph;
     }
 
     /// i文字目で終わるノードを探す
-    fn node_list(&self, i: usize) -> Option<Vec<Rc<RefCell<WordNode>>>> {
-        match self.graph.get(&i) {
-            Some(p) => Some(p.clone()),
-            None => None,
-        }
+    fn node_list(&self, i: i32) -> Option<&Vec<WordNode>> {
+        self.graph.get(&i)
     }
 
-    fn get_node_cost(&self, node: &Rc<RefCell<WordNode>>) -> f32 {
+    fn get_node_cost(&self, node: &WordNode) -> f32 {
         // 簡単のために、一旦スコアを文字列長とする。
         // 経験上、長い文字列のほうがあたり、というルールでもそこそこ変換できる。
         // TODO あとでちゃんと unigram のコストを使うよに変える。
-        // ここ明らかに書き方おかしいと思う。
-        unsafe {
-            return (*(node.as_ptr())).kanji.len() as f32;
-        }
+        return node.kanji.len() as f32;
     }
 
-    fn get_prev_nodes(&self, node: &Rc<RefCell<WordNode>>) -> Option<&Vec<Rc<RefCell<WordNode>>>> {
+    // -1  0  1 2
+    // BOS わ た し
+    //     [  ][ ]
+    //     [     ]
+    fn get_prev_nodes(&self, node: &WordNode) -> Option<&Vec<WordNode>> {
         // ここの処理を簡単にするために BOS が入っている、のだとおもう。
-        self.graph.get(&(node.as_ref().borrow().start_pos - 1))
+        trace!("get_prev_nodes: {}", node.start_pos - 1);
+        self.graph.get(&(node.start_pos))
     }
 
-    fn get_edge_cost(&self, prev: &Rc<RefCell<WordNode>>, node: &Rc<RefCell<WordNode>>) -> f32 {
+    fn get_edge_cost(&self, prev: &WordNode, node: &WordNode) -> f32 {
         // TODO: あとで実装する
         return 0.0;
     }
 
-    fn viterbi(&self, yomi: &String) {
-        for i in 0..yomi.len() {
-            let Some(nodes) = self.node_list(i) else {
+    fn viterbi(&self, yomi: &String) -> String {
+        let mut prevmap: HashMap<&WordNode, &WordNode> = HashMap::new();
+
+        for i in 1..yomi.len() + 2 {
+            let Some(nodes) = self.node_list(i as i32) else {
                 continue;
             };
             for mut node in nodes {
-                let node_cost = self.get_node_cost(&node);
+                let node_cost = self.get_node_cost(node);
                 let mut cost = f32::MAX;
                 let mut shortest_prev = None;
-                let prev_nodes = self.get_prev_nodes(&node).unwrap();
-                unsafe {
-                    for prev in prev_nodes.clone() {
-                        let edge_cost = self.get_edge_cost(&prev, &node);
-                        let tmp_cost = (*prev.as_ptr()).cost + edge_cost + node_cost;
-                        if tmp_cost < cost {
-                            cost = tmp_cost;
-                            shortest_prev = Some(prev);
-                        }
+                let prev_nodes = self.get_prev_nodes(&node).expect(
+                    format!(
+                        "Cannot get prev nodes for '{}' start={}",
+                        node.kanji, node.start_pos
+                    )
+                    .as_str(),
+                );
+                for prev in prev_nodes.clone() {
+                    let edge_cost = self.get_edge_cost(&prev, &node);
+                    let tmp_cost = prev.cost + edge_cost + node_cost;
+                    if tmp_cost < cost {
+                        cost = tmp_cost;
+                        shortest_prev = Some(prev);
                     }
                 }
-                node.get_mut().set_prev(shortest_prev);
-
-                let b = node.borrow_mut();
-                // let mut p = node.borrow_mut();
-                // p.get_mut().set_prev(shortest_prev);
-                // p.get_mut().cost = cost;
+                prevmap.insert(node, shortest_prev.unwrap());
             }
         }
+
+        let eos = self
+            .graph
+            .get(&((yomi.len() + 1) as i32))
+            .unwrap()
+            .get(0)
+            .unwrap();
+        let bos = self.graph.get(&0).unwrap().get(0).unwrap();
+        let mut node = eos;
+        let mut result: Vec<String> = Vec::new();
+        while node != bos {
+            if node.kanji != "__EOS__" {
+                result.push(node.kanji.to_string());
+            }
+            node = prevmap
+                .get(node)
+                .expect(format!("Cannot get previous node: {}", node.kanji).as_str());
+        }
+        result.reverse();
+        return result.join("");
     }
 }
 
@@ -276,5 +298,32 @@ mod tests {
                 (9, vec!["わたし".to_string(), "し".to_string()]),
             ])
         )
+    }
+
+    #[test]
+    fn test_resolver() {
+        env_logger::builder().is_test(true).try_init().unwrap();
+
+        let mut builder = KanaTrieBuilder::new();
+        builder.add(&"abc".to_string());
+        builder.add(&"ab".to_string());
+        builder.add(&"c".to_string());
+        let kana_trie = builder.build();
+
+        let graph_builder = Segmenter::new(kana_trie);
+        let graph = graph_builder.build(&"abc".to_string());
+        assert_eq!(
+            graph,
+            HashMap::from([
+                (2, vec!["ab".to_string()]),
+                (3, vec!["abc".to_string(), "c".to_string()]),
+            ])
+        );
+
+        // -1  0  1  2
+        // BOS a  b  c
+        let resolver = GraphResolver::new(&"abc".to_string(), graph);
+        let result = resolver.viterbi(&"abc".to_string());
+        assert_eq!(result, "abc");
     }
 }
