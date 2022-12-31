9bfa607089a18c1ac5b529f95a76ef92d6198c4f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-01-01 01:10:16 +0900

    fix

--- akaza-core/bin/akaza-demo/src/main.rs
+++ akaza-core/bin/akaza-demo/src/main.rs
@@ -1,26 +1,14 @@
 use std::env;
-use std::fs::File;
-use std::io::Write;
-
 
 use libakaza::akaza_builder::AkazaBuilder;
 use log::info;
 
-
-
-
-
-
-
-
-
-
-fn dump_dot(fname: &str, dot: &str) {
-    info!("Writing {}", fname);
-    let mut file = File::create(fname).unwrap();
-    file.write_all(dot.as_bytes()).unwrap();
-    file.sync_all().unwrap();
-}
+// fn dump_dot(fname: &str, dot: &str) {
+//     info!("Writing {}", fname);
+//     let mut file = File::create(fname).unwrap();
+//     file.write_all(dot.as_bytes()).unwrap();
+//     file.sync_all().unwrap();
+// }
 
 fn main() -> anyhow::Result<()> {
     env_logger::init_from_env(
