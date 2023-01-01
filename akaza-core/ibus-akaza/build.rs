84de50941cbcfc44a4f13ab3b188e09a02ab5da3
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-01-02 08:10:54 +0900

    wrapper

---
+++ akaza-core/ibus-akaza/build.rs
@@ -0,0 +1,10 @@
+extern crate cc;
+
+fn main() {
+    cc::Build::new()
+        .file("wrapper.c")
+        .include("wrapper")
+        .compile("wrapper");
+
+    println!("cargo:rustc-link-lib=ibus");
+}
---
+++ akaza-core/ibus-akaza/wrapper.c
@@ -0,0 +1,3 @@
+#include <cstring>
+#include <cstdlib>
+
