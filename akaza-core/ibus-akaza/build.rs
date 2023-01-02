17d1faa7365025ab236a109d9cbc3d38599a250c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-01-02 09:00:17 +0900

    compile

--- akaza-core/ibus-akaza/build.rs
+++ akaza-core/ibus-akaza/build.rs
@@ -1,10 +1,33 @@
 extern crate cc;
 
-fn main() {
-    cc::Build::new()
-        .file("wrapper.c")
-        .include("wrapper")
-        .compile("wrapper");
+use std::io::Read;
+use std::process::{Command, Stdio};
 
+fn pkgconfig(module: &str) -> Vec<String> {
+    let child = Command::new("pkg-config")
+        .arg(module)
+        .arg("--cflags")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to spawn child process");
+    let mut buf = String::new();
+    child.stdout.unwrap().read_to_string(&mut buf).unwrap();
+    let args: Vec<&str> = buf.trim().split(' ').collect();
+    args.iter().map(|f| f.to_string()).collect()
+}
+
+fn main() {
     println!("cargo:rustc-link-lib=ibus-1.0");
+    println!("cargo:rustc-link-lib=enchant-2");
+    println!("cargo:rerun-if-changed=wrapper.c");
+
+    let mut p = cc::Build::new();
+    let mut c = p.file("wrapper.c");
+    c = c.include("wrapper");
+    for module in &["ibus-1.0", "enchant-2"] {
+        for flag in pkgconfig(module) {
+            c = c.flag(flag.as_str());
+        }
+    }
+    p.compile("wrapper");
 }
---
+++ akaza-core/ibus-akaza/config.h
@@ -0,0 +1,2 @@
+// TODO: Generate file from config.h.in
+#define PKGDATADIR "/usr/share/ibus-akaza/"
