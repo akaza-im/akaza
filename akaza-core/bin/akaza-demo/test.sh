083fc28921cea3b80f947e5fd9b4753128dfe990
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-01-05 14:21:18 +0900

    quote

--- akaza-core/bin/akaza-demo/test.sh
+++ akaza-core/bin/akaza-demo/test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")
-DATADIR=$(readlink -f $BASEDIR/../../../akaza-data/data/)
+DATADIR=$(readlink -f "$BASEDIR/../../../akaza-data/data/")
 RUST_BACKTRACE=1
 set -ex
-cargo run $DATADIR わたし
-cargo run $DATADIR わたしのなまえはなかのです
+cargo run "$DATADIR" わたし
+cargo run "$DATADIR" わたしのなまえはなかのです
+cargo run "$DATADIR" きょう
