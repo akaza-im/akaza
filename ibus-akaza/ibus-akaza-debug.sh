46690323f52136ff12d2bc8ef6f1ffe626b0fdcf
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-01-06 10:04:58 +0900

    change file permission for better security

--- ibus-akaza/ibus-akaza-debug.sh
+++ ibus-akaza/ibus-akaza-debug.sh
@@ -3,11 +3,11 @@ set -ex
 
 BASEDIR=$(dirname "$0")
 
+umask 077
+
 exec 1>> ~/.ibus-akaza.log
 exec 2>&1
 
 export RUST_BACKTRACE=4
-# export RUST_LOG=trace
-export RUST_LOG=info
 
-exec $BASEDIR/../target/debug/ibus-akaza --ibus
+exec $BASEDIR/../target/debug/ibus-akaza --ibus -vv
