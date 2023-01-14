c848f5ae94cb9451a1523c6f40cef221314581bb
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-01-15 01:08:11 +0900

    add test case

--- libakaza/src/skk/merge_skkdict.rs
+++ libakaza/src/skk/merge_skkdict.rs
@@ -30,3 +30,29 @@ pub fn merge_skkdict(dicts: Vec<HashMap<String, Vec<String>>>) -> HashMap<String
 
     result
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_merge_skkdict() {
+        let got = merge_skkdict(vec![
+            HashMap::from([("ご".to_string(), vec!["語".to_string()])]),
+            HashMap::from([
+                ("ご".to_string(), vec!["後".to_string(), "碁".to_string()]),
+                ("お".to_string(), vec!["緒".to_string()]),
+            ]),
+        ]);
+        assert_eq!(
+            got,
+            HashMap::from([
+                (
+                    "ご".to_string(),
+                    vec!["語".to_string(), "後".to_string(), "碁".to_string()]
+                ),
+                ("お".to_string(), vec!["緒".to_string()])
+            ])
+        );
+    }
+}
