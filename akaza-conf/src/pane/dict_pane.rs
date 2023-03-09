1f88596a7d523da4ac676555ba1e3e93319aec20
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-03-09 22:53:24 +0900

    update deps

--- akaza-conf/src/pane/dict_pane.rs
+++ akaza-conf/src/pane/dict_pane.rs
@@ -1,7 +1,6 @@
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 
-use gtk4::builders::MessageDialogBuilder;
 use gtk4::prelude::ButtonExt;
 use gtk4::prelude::ComboBoxExt;
 use gtk4::prelude::DialogExt;
@@ -12,10 +11,7 @@ use gtk4::prelude::FileExt;
 use gtk4::prelude::GridExt;
 use gtk4::prelude::GtkWindowExt;
 use gtk4::prelude::WidgetExt;
-use gtk4::{
-    Button, ComboBoxText, FileChooserAction, FileChooserDialog, Grid, Label, MessageType,
-    ResponseType, ScrolledWindow, Text, TextBuffer, TextView, Window,
-};
+use gtk4::{Button, ComboBoxText, FileChooserAction, FileChooserDialog, Grid, Label, MessageDialog, MessageType, ResponseType, ScrolledWindow, Text, TextBuffer, TextView, Window};
 use log::info;
 
 use libakaza::config::{Config, DictConfig, DictEncoding, DictType, DictUsage};
@@ -257,7 +253,7 @@ fn build_add_user_dict_btn(dict_list_grid: Grid, config: Arc<Mutex<Config>>) -> 
                     window.close();
                 }
                 Err(err) => {
-                    let dialog = MessageDialogBuilder::new()
+                    let dialog = MessageDialog::builder()
                         .message_type(MessageType::Error)
                         .text(&format!("Error: {err}"))
                         .build();
